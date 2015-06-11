12f5b3765716dabb63ddf331b07a18d2c2d4e78c
David Han <davidhan527@gmail.com>
2015-06-10 21:40:57 -0400

    getting instagram results using coordinates

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,14 +1,30 @@
 class RestaurantsController < ApplicationController
+  before_action :connect_to_instagram, only: :index
+
   def index
 
   end
 
   def instagram_results
-    puts "========================================================="
+    results = Instagram.media_search(lat, long, count: 100)
     respond_to do |format|
       format.json {}
     end
 
     redirect_to :restaurants
+  end
+
+  private
+
+  def connect_to_instagram
+    client = Instagram.client(:access_token => session[:access_token])
+  end
+
+  def lat
+    params[:latitude].to_s
+  end
+
+  def long
+    params[:longitude].to_s
   end
 end
